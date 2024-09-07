d6e2aec552f4146544d7866455e66846bd9be455
atiqurrahmanpk <info.atiqurrahmanpk@gmail.com>
2024-09-07 22:43:17 +0600

    array practice

---
+++ Class_3/array.php
@@ -0,0 +1,87 @@
+<?php
+
+//========== Index Array==========
+
+// $users = array(            // item no 1
+//     "Atiqur rahman",
+//     "Masum Billah",
+//     "A Rahman Pk"
+// );
+
+// echo "<pre>";
+//  print_r($users) ;
+
+//  print_r($users[1]) ;
+
+// or
+// var_dump ($users);
+
+// echo "</pre>";
+
+//========OR================
+
+// $users = [                // Item no 2
+//     "Atiqur rahman",
+//     "Masum Billah",
+//     "A Rahman Pk"
+// ];
+
+// echo "<pre>";
+// //  print_r($users) ;
+
+//  print_r($users[2]) ;
+
+// // or
+// //var_dump ($users);
+
+// echo "</pre>";
+
+//================ Assosiative Array =============
+
+// $Your_name1 = "Hasan Iqbal";
+// $Your_name2 = "Khaled Mahmud";
+// $users = [                // Item no 2
+//     "first_Name"=> "Atiqur rahman",
+//     "last_name" => "Masum Billah",
+//     "A Rahman Pk",
+//     8 => "Hasan Mahbub",
+//     "al amin Islam",
+//     3 => "Masudur Rahman", // 3 and 3.1 ekoi result hobe
+//     "3" => "Babul Akter" ,     // Masudur k change korse and 3 number hisebe use hoise
+//     "first_name" => " A Rahman Pk",  // first name k change korse
+
+//     1 => "Kolijar vai",
+//     true => "Adorer vai", // true = 1 , kolijar vai change kore adorer vai korese
+    
+//     "your_name" => $Your_name1, $Your_name2   // Bahirer Veriable newa jai
+
+// ];
+
+// echo "<pre>";
+// //  print_r($users) ;
+
+//  print_r($users) ;
+
+// // or
+// //var_dump ($users);
+
+// echo "</pre>";
+//======================================
+
+// $num_s = [12, 14, 15, 17, 18, 20, 21, 24];
+
+
+// echo "<pre>";
+// print_r($num_s) ;
+
+// //print_r(count($num_s));
+
+// // or
+// //var_dump (count($num_s));
+
+// echo "</pre>";
+
+
+
+
+
